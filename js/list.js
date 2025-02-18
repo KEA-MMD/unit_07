c83f946b3c620ff0950aeff069c96e5ca4c644d0
Tilde <tildews@gmail.com>
2025-02-18 23:27:30 +0100

    dynamic list

---
+++ js/list.js
@@ -0,0 +1,36 @@
+const recipeList = document.querySelector(".list_flex");
+
+function collectData() {
+  console.log("script loaded collectData");
+  fetch("https://dummyjson.com/recipes")
+    .then((res) => res.json())
+    .then(showRecipes);
+}
+
+function showRecipes(data) {
+  console.log("showRecipes");
+
+  const allRecipes = data.recipes;
+  let arrayWithRecipes = allRecipes.map(
+    (recipe) =>
+      `
+    <div class="list_item">
+          <div class="item_description">
+            <p>${recipe.cuisine}</p>
+            <h2>${recipe.name}</h2>
+            <p>TIME: ${recipe.prepTimeMinutes} + ${recipe.cookTimeMinutes} min.</p>
+            <p>SERVINGS: ${recipe.servings}</p>
+            <p>${recipe.difficulty}</p>
+          </div>
+          <img
+            src="${recipe.image}"
+            alt="Recipe image"
+          />
+        </div>
+    `
+  );
+
+  recipeList.innerHTML = arrayWithRecipes.join("");
+}
+
+collectData();
